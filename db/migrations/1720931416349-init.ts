28693a05a97e8ffb024c84b96e0977a6414068ef
t.filiniuk <t.filiniuk@gmail.com>
2024-07-14 07:30:46 +0300

    generate migrations 2

--- db/migrations/1720931416349-init.ts
+++ db/migrations/1720931416349-init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1720930797105 implements MigrationInterface {
-    name = 'Init1720930797105'
+export class Init1720931416349 implements MigrationInterface {
+    name = 'Init1720931416349'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "twoFASecret" text, "enable2FA" boolean NOT NULL DEFAULT false, "apiKey" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
